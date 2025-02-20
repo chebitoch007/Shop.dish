80ca19b9ca1c3ec8fefca72632ce9d857976c221
chebitoch007 <eliphazchebitoch@gmail.com>
2025-02-20 13:35:57 +0300

    django commit

---
+++ shopapp/urls.py
@@ -0,0 +1,17 @@
+
+from django.contrib import admin
+from django.urls import path
+from shopapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('gallery/', views.images, name='gallery'),
+    path('about/', views.about, name='about'),
+    path('form/', views.form, name='sign in'),
+    path('products/', views.products, name='Products'),
+]
+
+
+
+
---
+++ shopapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+def images(request):
+    return render(request,'gallery.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def form(request):
+    return render(request,'form.html')
+
+def products(request):
+    return render(request,'products.html')
